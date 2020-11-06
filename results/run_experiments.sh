2520d346b09983835324975204297aed7d6b2f0d
Alexander Dietmüller <alexander@dietmueller.de>
2020-11-06 14:49:13 +0000

    Complete experiment setup and add script to run.

---
+++ results/run_experiments.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+function run {
+    # Run with some default settings
+    cmd="NS_GLOBAL_VALUE=\"RngRun=$1\" waf --run \"shift ${@:2} --apps=20 --apprate=100kbps --linkrate=3Mbps\""
+    echo $cmd
+    eval "$cmd"
+}
+
+# Repeat ten times
+for run in {0..9}; do
+
+# Simulate different traffic mixes
+run $run --prefix="w1_$run" --w1=1 --w2=0 --w3=0 --congestion=0
+exit
+run $run --prefix="w2_$run" --w1=2 --w2=0 --w3=0 --congestion=0
+run $run --prefix="w3_$run" --w1=3 --w2=0 --w3=0 --congestion=0
+
+run $run --prefix="w1w2_$run" --w1=0.5 --w2=0.5 --w3=0 --congestion=0
+run $run --prefix="w1w3_$run" --w1=0.5 --w2=0 --w3=0.5 --congestion=0
+
+run $run --prefix="skewed1_$run" --w1=0.6 --w2=0.3 --w3=0.1 --congestion=0
+run $run --prefix="skewed3_$run" --w1=0.1 --w2=0.3 --w3=0.6 --congestion=0
+
+# Also simulate congestion levels
+run $run --prefix="c1_$run" --w1=1 --w2=0 --w3=0 --congestion=1Mbps
+run $run --prefix="c2_$run" --w1=1 --w2=0 --w3=0 --congestion=2Mbps
+run $run --prefix="c3_$run" --w1=1 --w2=0 --w3=0 --congestion=3Mbps
+
+done
--- simulation/shift.cc
+++ simulation/shift.cc
@@ -44,13 +44,6 @@ NS_LOG_COMPONENT_DEFINE("MiniTopology");
 const auto TCP = TypeIdValue(TcpSocketFactory::GetTypeId());
 const auto UDP = TypeIdValue(UdpSocketFactory::GetTypeId());
 
-void RTTMeasurement(Ptr<OutputStreamWrapper> stream, uint32_t burst, Time rtt)
-{
-    *stream->GetStream() << Simulator::Now().GetSeconds() << ','
-                         << burst << ','
-                         << rtt.GetSeconds() << std::endl;
-}
-
 // Put the current timestamp and packet size into a log.
 void logSize(Ptr<OutputStreamWrapper> stream, Ptr<Packet const> p)
 {
@@ -108,7 +101,7 @@ void setTimeTag(Ptr<Packet const> p)
 };
 
 // If the packet is tagged with a timestamp, write current time and tag
-// into the stream.
+// into the stream. Also add packet size.
 void logTimeTag(Ptr<OutputStreamWrapper> stream, Ptr<Packet const> p)
 {
     TimestampTag tag;
@@ -117,11 +110,12 @@ void logTimeTag(Ptr<OutputStreamWrapper> stream, Ptr<Packet const> p)
         auto current_time = Simulator::Now();
         auto diff_time = current_time - tag.GetTime();
         *stream->GetStream() << current_time.GetSeconds() << ','
-                             << diff_time.GetSeconds() << std::endl;
+                             << diff_time.GetSeconds() << ','
+                             << p->GetSize() << std::endl;
     }
     else
     {
-        NS_LOG_DEBUG("Packet without timestamp.");
+        //NS_LOG_DEBUG("Packet without timestamp, won't log.");
     };
 };
 
@@ -162,17 +156,52 @@ int main(int argc, char *argv[])
     // run-time, via command-line arguments
     //
 
-    double interval = 1.0;
-    u_int32_t burstsize = 1;
+    int n_apps = 10;
+    DataRate linkrate("5Mbps");
+    DataRate baserate("100kbps");
+    DataRate congestion("0Mbps");
+    std::string basedir = "/workspaces/ns3-fitnets/distributions/";
+    std::string w1 = basedir + "Facebook_WebServerDist_IntraCluster.txt";
+    std::string w2 = basedir + "DCTCP_MsgSizeDist.txt";
+    std::string w3 = basedir + "Facebook_HadoopDist_All.txt";
+    std::string prefix = "shift";
+    double c_w1 = 1;
+    double c_w2 = 1;
+    double c_w3 = 1;
 
     CommandLine cmd;
-    cmd.AddValue("interval", "Seconds to wait between probes.", interval);
-    cmd.AddValue("burstsize", "Number of probing packets.", burstsize);
+    cmd.AddValue("apps", "Number of traffic apps per workload.", n_apps);
+    cmd.AddValue("apprate", "Base traffic rate for each app.", baserate);
+    cmd.AddValue("linkrate", "Link capacity rate.", linkrate);
+    cmd.AddValue("w1", "Factor for W1 traffic (FB webserver).", c_w1);
+    cmd.AddValue("w2", "Factor for W2 traffic (DCTCP messages).", c_w2);
+    cmd.AddValue("w3", "Factor for W3 traffic (FB hadoop).", c_w3);
+    cmd.AddValue("congestion", "Congestion traffic rate.", congestion);
+    cmd.AddValue("prefix", "Prefix for log files.", prefix);
     cmd.Parse(argc, argv);
+
+    // Compute resulting workload datarates.
+    auto rate_w1 = DataRate(static_cast<uint64_t>(c_w1 * baserate.GetBitRate()));
+    auto rate_w2 = DataRate(static_cast<uint64_t>(c_w2 * baserate.GetBitRate()));
+    auto rate_w3 = DataRate(static_cast<uint64_t>(c_w3 * baserate.GetBitRate()));
+
+    /*
+    // Print Overview of seetings
+    NS_LOG_DEBUG("Overview:"
+                 "Apps: "
+                 << n_apps << std::endl
+                 << "Workloads (data rates per app):"
+                 << std::endl
+                 << "W1: " << w1 << " (" << rate_w1 << ")"
+                 << std::endl
+                 << "W2: " << w2 << " (" << rate_w2 << ")"
+                 << std::endl
+                 << "W3: " << w3 << " (" << rate_w3 << ")");
+    */
 
     // Simulation variables
     auto simStart = TimeValue(Seconds(0));
-    auto stopTime = Seconds(60);
+    auto stopTime = Seconds(30);
     auto simStop = TimeValue(stopTime);
 
     //
@@ -194,7 +223,7 @@ int main(int argc, char *argv[])
     NS_LOG_INFO("Build Topology");
     CsmaHelper csma;
     csma.SetChannelAttribute("FullDuplex", BooleanValue(true));
-    csma.SetChannelAttribute("DataRate", StringValue("5Mbps"));
+    csma.SetChannelAttribute("DataRate", DataRateValue(linkrate));
     csma.SetChannelAttribute("Delay", TimeValue(MilliSeconds(5)));
 
     // Create the csma links
@@ -207,27 +236,6 @@ int main(int argc, char *argv[])
     BridgeHelper bridge;
     bridge.Install(switchA, GetNetDevices(switchA));
     bridge.Install(switchB, GetNetDevices(switchB));
-
-    NS_LOG_INFO("Install Tracing");
-    AsciiTraceHelper asciiTraceHelper;
-
-    // Packet size.
-    std::stringstream sizefilename;
-    sizefilename << "sizes.csv";
-    auto sizefile = asciiTraceHelper.CreateFileStream(sizefilename.str());
-    sender->GetDevice(0)->TraceConnectWithoutContext(
-        "MacTx", MakeBoundCallback(&logSize, sizefile));
-
-    // Log (one-way) delay from sender to receiver (excludes other sources).
-    std::stringstream trackfilename;
-    //TODO: custom file name?
-    //trackfilename << "delays[" << interval << "," << burstsize << "].csv";
-    trackfilename << "delays.csv";
-    auto trackfile = asciiTraceHelper.CreateFileStream(trackfilename.str());
-    sender->GetDevice(0)->TraceConnectWithoutContext(
-        "MacTx", MakeCallback(&setTimeTag));
-    receiver->GetDevice(0)->TraceConnectWithoutContext(
-        "MacRx", MakeBoundCallback(&logTimeTag, trackfile));
 
     // Add internet stack and IP addresses to the hosts
     NS_LOG_INFO("Setup stack and assign IP Addresses.");
@@ -242,116 +250,91 @@ int main(int argc, char *argv[])
     ipv4.SetBase("10.1.1.0", "255.255.255.0");
     auto addresses = ipv4.Assign(hostDevices);
     // Get Address: Device with index 0/1, address 0 (only one address)
-    auto addrSender = addresses.GetAddress(0, 0);
     auto addrReceiver = addresses.GetAddress(1, 0);
-    // auto addrDisturbance = addresses.GetAddress(1, 0);
-
-    /*
-    // Create the probing client and server apps
-    NS_LOG_INFO("Create Probing Applications.");
-    uint16_t port = 9; // Discard port (RFC 863)
-
-    auto probing_client = CreateObjectWithAttributes<ProbingClient>(
-        "RemoteAddress", AddressValue(addrReceiver),
-        "RemotePort", UintegerValue(port),
-        "Interval", TimeValue(Seconds(interval)),
-        "Burstsize", UintegerValue(burstsize),
-        "StartTime", simStart,
-        "StopTime", simStop);
-    auto probing_server = CreateObjectWithAttributes<ProbingServer>(
-        "Port", UintegerValue(port),
-        "StartTime", simStart,
-        "StopTime", simStop);
-
-    // Install apps
-    h0->AddApplication(probing_client);
-    h1->AddApplication(probing_server);
-    */
 
     NS_LOG_INFO("Create Traffic Applications.");
-    //TODO: Turn into function?
-    // Create applications that send traffic in both directions
-    auto n_apps = 20;          //20;
     uint16_t base_port = 4200; // Note: We need two ports per pair
     auto trafficStart = TimeStream(1, 2);
     for (auto i_app = 0; i_app < n_apps; ++i_app)
     {
         // Addresses
-        auto appAddrSender = AddressValue(
-            InetSocketAddress(addrSender, base_port + i_app));
-        auto appAddrReceiver = AddressValue(
-            InetSocketAddress(addrReceiver, base_port + i_app));
+        auto port = base_port + i_app;
+        auto recvAddr = AddressValue(InetSocketAddress(addrReceiver, port));
 
         // Sink
         Ptr<Application> sink = CreateObjectWithAttributes<PacketSink>(
-            "Local", appAddrReceiver, "Protocol", TCP,
+            "Local", recvAddr, "Protocol", TCP,
             "StartTime", simStart, "StopTime", simStop);
         receiver->AddApplication(sink);
 
-        // Sources
-        /*
-        Ptr<Application> sourceSender = CreateObjectWithAttributes<OnOffApplication>(
-            "Remote", appAddrReceiver, "Protocol", TCP,
-            "OnTime", TimeStreamValue(), "OffTime", TimeStreamValue(),
-            "DataRate", DataRateValue(DataRate("2Mbps")),
-            "StartTime", TimeValue(Seconds(trafficStart->GetValue())),
-            "StopTime", simStop);
-        */
-        Ptr<CdfApplication> source = CreateObjectWithAttributes<CdfApplication>(
-            "Remote", appAddrReceiver, "Protocol", TCP,
-            "DataRate", DataRateValue(DataRate("100kbps")),
-            "CdfFile", StringValue("/workspaces/ns3-fitnets/distributions/DCTCP_MsgSizeDist.txt"),
-            "StartTime", TimeValue(Seconds(trafficStart->GetValue())),
-            "StopTime", simStop);
-        source->AssignStreams(2 * i_app); // two streams each.
-        sender->AddApplication(source);
+        // Sources for each workload
+
+        if (rate_w1 > 0)
+        {
+            Ptr<CdfApplication> source1 = CreateObjectWithAttributes<CdfApplication>(
+                "Remote", recvAddr, "Protocol", TCP,
+                "DataRate", DataRateValue(rate_w1), "CdfFile", StringValue(w3),
+                "StartTime", TimeValue(Seconds(trafficStart->GetValue())),
+                "StopTime", simStop);
+            sender->AddApplication(source1);
+        }
+        if (rate_w2 > 0)
+        {
+            Ptr<CdfApplication> source2 = CreateObjectWithAttributes<CdfApplication>(
+                "Remote", recvAddr, "Protocol", TCP,
+                "DataRate", DataRateValue(rate_w2), "CdfFile", StringValue(w2),
+                "StartTime", TimeValue(Seconds(trafficStart->GetValue())),
+                "StopTime", simStop);
+            sender->AddApplication(source2);
+        }
+        if (rate_w3 > 0)
+        {
+            Ptr<CdfApplication> source3 = CreateObjectWithAttributes<CdfApplication>(
+                "Remote", recvAddr, "Protocol", TCP,
+                "DataRate", DataRateValue(rate_w3), "CdfFile", StringValue(w3),
+                "StartTime", TimeValue(Seconds(trafficStart->GetValue())),
+                "StopTime", simStop);
+            sender->AddApplication(source3);
+        }
     }
 
-    /*
     NS_LOG_INFO("Configure Congestion App.");
     // Just blast UDP traffic from time to time
     Ptr<Application> congestion_sink = CreateObjectWithAttributes<PacketSink>(
-        "Local", AddressValue(InetSocketAddress(h1_address, 2100)),
+        "Local", AddressValue(InetSocketAddress(addrReceiver, 2100)),
         "Protocol", UDP, "StartTime", simStart, "StopTime", simStop);
-    h1->AddApplication(congestion_sink);
+    receiver->AddApplication(congestion_sink);
 
     Ptr<Application> congestion_source = CreateObjectWithAttributes<OnOffApplication>(
-        "Remote", AddressValue(InetSocketAddress(h1_address, 2100)),
+        "Remote", AddressValue(InetSocketAddress(addrReceiver, 2100)),
         "Protocol", UDP,
-        // Shorter and rarer bursts
-        "OnTime", TimeStreamValue(0.1, 0.5),
-        "OffTime", TimeStreamValue(2, 4),
-        "DataRate", DataRateValue(DataRate("10Mbps")),
+        "OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"),
+        "OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"),
+        "DataRate", DataRateValue(congestion),
         "StartTime", TimeValue(Seconds(trafficStart->GetValue())),
         "StopTime", simStop);
-    h0->AddApplication(congestion_source);
-    */
+    disturbance->AddApplication(congestion_source);
 
-    NS_LOG_INFO("Configure Tracing.");
-    /*
-    std::stringstream rtt_file;
-    rtt_file << "rtt[" << interval << "," << burstsize << "].csv";
-    probing_client->TraceConnectWithoutContext(
-        "RTT",
-        MakeBoundCallback(
-            &RTTMeasurement,
-            asciiTraceHelper.CreateFileStream(rtt_file.str())));
-    */
-    //
-    // Configure tracing of all enqueue, dequeue, and NetDevice receive events.
-    // Trace output will be sent to the file "csma-bridge.tr"
-    //
-    //AsciiTraceHelper ascii;
-    //csma.EnableAsciiAll(ascii.CreateFileStream("csma-bridge.tr"));
+    NS_LOG_INFO("Install Tracing");
+    AsciiTraceHelper asciiTraceHelper;
 
-    //
-    // Also configure some tcpdump traces; each interface will be traced.
-    // The output files will be named:
-    //     csma-bridge-<nodeId>-<interfaceId>.pcap
-    // and can be read by the "tcpdump -r" command (use "-tt" option to
-    // display timestamps correctly)
-    //
-    csma.EnablePcapAll("csma-bridge", false);
+    // Packet size.
+    std::stringstream sizefilename;
+    sizefilename << prefix << "_sizes.csv";
+    auto sizefile = asciiTraceHelper.CreateFileStream(sizefilename.str());
+    sender->GetDevice(0)->TraceConnectWithoutContext(
+        "MacTx", MakeBoundCallback(&logSize, sizefile));
+
+    // Log (one-way) delay from sender to receiver (excludes other sources).
+    std::stringstream trackfilename;
+    trackfilename << prefix << "_delays.csv";
+    auto trackfile = asciiTraceHelper.CreateFileStream(trackfilename.str());
+    sender->GetDevice(0)->TraceConnectWithoutContext(
+        "MacTx", MakeCallback(&setTimeTag));
+    receiver->GetDevice(0)->TraceConnectWithoutContext(
+        "MacRx", MakeBoundCallback(&logTimeTag, trackfile));
+
+    //csma.EnablePcapAll("csma-bridge", false);
 
     //
     // Now, do the actual simulation.
