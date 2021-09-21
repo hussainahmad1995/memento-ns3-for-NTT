3a3e844f4156b50c4dfd4e65019f6bec7b5af8e0
Alexander Dietmüller <adietmue@ethz.ch>
2021-09-21 15:48:51 +0000

    Parallelize run_experiment.

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,41 +3,54 @@
 resultdir="results"
 mkdir -p "$resultdir"
 
+
+# Repeat 22 times with different RNG to have plenty of data.
+maxrun=21
+
+# Parallelization
+procs=${1:-1}  # 1 process per default
+
+
 # Build modules once, then run without building.
 waf build
 
-function run {
+function exprun {
+    # First arg: rng run, secong arg: experiment name.
     # Run with some default settings
-    cmd="NS_GLOBAL_VALUE=\"RngRun=$1\" waf --run-no-build \"trafficgen ${@:3} --apps=20 --apprate=1Mbps --linkrate=30Mbps --prefix=$resultdir/$2\""
-    echo "$cmd"
-    eval "$cmd"
-    #(cd $resultdir && eval "$cmd")
+    cmd="NS_GLOBAL_VALUE=\"RngRun=$1\" waf --run-no-build \"trafficgen ${@:3} --apps=20 --apprate=1Mbps --linkrate=30Mbps --prefix=$resultdir/$2_$1\""
+    echo "STARTING $2($1)"
+    output=$(eval $cmd 2>&1)
+    output="OUTPUT $2($1)\n${output}\n"
+    printf "$output"
+}
+export -f exprun   # make it visible for xargs
+export resultdir   # same same
+
+function run {  # First argument is experiment name, others are passed through.
+    seq 0 $maxrun | xargs --max-procs $procs -n1 -I % bash -c "exprun % $@"
 }
 
-# Repeat 22 times with different RNG to have plenty of data.
-for run in {0..21}; do
 
 # Simulate different traffic mixes
-run $run "w1_$run" --w1=1 --w2=0 --w3=0 --congestion=0
-run $run "w2_$run" --w1=0 --w2=1 --w3=0 --congestion=0
-run $run "w3_$run" --w1=0 --w2=0 --w3=1 --congestion=0
-
-run $run "w1w2_$run" --w1=0.5 --w2=0.5 --w3=0 --congestion=0
-run $run "w1w3_$run" --w1=0.5 --w2=0 --w3=0.5 --congestion=0
-
-run $run "skewed1_$run" --w1=0.6 --w2=0.3 --w3=0.1 --congestion=0
-run $run "skewed3_$run" --w1=0.1 --w2=0.3 --w3=0.6 --congestion=0
+run "w1" --w1=1 --w2=0 --w3=0 --congestion=0
+run "w2" --w1=0 --w2=1 --w3=0 --congestion=0
+run "w3" --w1=0 --w2=0 --w3=1 --congestion=0
 
 # Also simulate congestion levels
 # 100% average network utilization
-run $run "w1_c100_$run" --w1=1 --w2=0 --w3=0 --congestion=10Mbps
-run $run "w2_c100_$run" --w1=0 --w2=1 --w3=0 --congestion=10Mbps
-run $run "w3_c100_$run" --w1=0 --w2=0 --w3=1 --congestion=10Mbps
+run "w1_c100" --w1=1 --w2=0 --w3=0 --congestion=10Mbps
+run "w2_c100" --w1=0 --w2=1 --w3=0 --congestion=10Mbps
+run "w3_c100" --w1=0 --w2=0 --w3=1 --congestion=10Mbps
 
-# 150% average network utilization
-run $run "w1_c133_$run" --w1=1 --w2=0 --w3=0 --congestion=20Mbps
-run $run "w2_c133_$run" --w1=0 --w2=1 --w3=0 --congestion=20Mbps
-run $run "w3_c133_$run" --w1=0 --w2=0 --w3=1 --congestion=20Mbps
+# 133% average network utilization
+run "w1_c133" --w1=1 --w2=0 --w3=0 --congestion=20Mbps
+run "w2_c133" --w1=0 --w2=1 --w3=0 --congestion=20Mbps
+run "w3_c133" --w1=0 --w2=0 --w3=1 --congestion=20Mbps
 
 
-done
+# Some extra tests, not sure if needed.
+run "w1w2" --w1=0.5 --w2=0.5 --w3=0 --congestion=0
+run "w1w3" --w1=0.5 --w2=0 --w3=0.5 --congestion=0
+
+run "skewed1" --w1=0.6 --w2=0.3 --w3=0.1 --congestion=0
+run "skewed3" --w1=0.1 --w2=0.3 --w3=0.6 --congestion=0
